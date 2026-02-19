315a88d130cbb8c41eb47c65d5ac25163048e02a
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-19 14:22:34 -0300

    feat: inicia repositorio

---
+++ src/main/java/com/weg/sis_biblioteca/SisBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.weg.sis_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisBibliotecaApplication.class, args);
+	}
+
+}
